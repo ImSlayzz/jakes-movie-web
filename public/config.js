ad39514910182570e47e1e579dd334e1f1482adb
ImSlayzz <156510080+ImSlayzz@users.noreply.github.com>
2024-04-01 22:48:41 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,13 +1,13 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://mw-backend.lonelil.com",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NzQ1ZWVkOTE1NDZkMWM5YWJiNjFiYzIyMjcxNDI4NiIsInN1YiI6IjY1YjZmZjRjMTA4OWJhMDE2NGY3OGQyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UsPCiyMsQFv0BwAjrlHMtUYke8z8P7M49uk6iJygZUc",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
-  VITE_DMCA_EMAIL: null,
+  VITE_DMCA_EMAIL: slayzz@fbi.ac,
 
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
   VITE_NORMAL_ROUTER: false,
